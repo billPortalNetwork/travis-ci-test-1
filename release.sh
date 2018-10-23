11f5427ca570c4451e83c3f2f7fec98dca7df7d5
billPortalNetwork <bill@portal.network>
2018-10-23 10:29:05 +0800

    Add log

--- release.sh
+++ release.sh
@@ -1,4 +1,7 @@
 git checkout master	
-npm version patch	
+echo "Version update" 
+npm version patch
+echo "Commit"
 git commit --amend -m  "[ci skip] version changed"	
+echo "Push"
 git push https://${GIT_ACCOUNT}:${GIT_PW}@github.com/billPortalNetwork/travis-ci-test-1.git master
